6e0bcec6578c9f07623bb9e4ffc3b2a0078757b8
Kirato Yoshihara <36755839+kira1228@users.noreply.github.com>
2025-04-16 23:13:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class Model(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 20, 5)
         self.conv2 = nn.Conv2d(20, 20, 5)
+        self.conv3 = nn.Conv2d(20, 30, 5)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
